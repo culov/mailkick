099e8067eb245ef9ecf618cba0f63e6dddb4d437
Ivan Pardo <ivanpardo@Ivans-MacBook-Pro.local>
2023-12-25 19:17:43 -0800

    mailkick

--- lib/mailkick/engine.rb
+++ lib/mailkick/engine.rb
@@ -9,8 +9,8 @@ module Mailkick
         creds =
           if app.respond_to?(:credentials) && app.credentials.secret_key_base
             app.credentials
-          elsif app.respond_to?(:secrets)
-            app.secrets
+          # elsif app.respond_to?(:secrets)
+          #   app.secrets
           else
             app.config
           end
